23a932311438b914dd36b7d85719d20de60836b9
disa4148 <denis.nikolaenko.2004@mail.ru>
2024-05-03 23:32:58 +0700

    fix

--- src/shared/ui/miniNft/mini-nft.tsx
+++ src/shared/ui/miniNft/mini-nft.tsx
@@ -1,31 +1,54 @@
 import css from './miniNft.module.scss';
 import Image from 'next/image';
-export default function MiniNft(): JSX.Element {
+
+type Props = {
+  name: string;
+  price: number;
+  total: number;
+  percentage: number;
+};
+
+export default function MiniNft({
+  name,
+  percentage,
+  price,
+  total,
+}: Props): JSX.Element {
   return (
     <div className={css.wrapper}>
-        <div className={css.leftItems}>
-            <Image src={'/assets/forTest/tgNft.png'} alt='NFT' width={65} height={60}/>
+      <div className={css.leftItems}>
+        <Image
+          src={'/assets/forTest/tgNft.png'}
+          alt="NFT"
+          width={65}
+          height={60}
+        />
+      </div>
+      <div className={css.middleItems}>
+        <div>
+          <h3>{name}</h3>
+          <Image
+            src={'/assets/icons/verified.svg'}
+            alt="Verified"
+            width={11}
+            height={11}
+          />
         </div>
-        <div className={css.middleItems}>
-            <div>
-                <h3>Telegram Usernames</h3>
-                <Image src={'/assets/icons/verified.svg'} alt='Verified' width={11} height={11}/>
-            </div>
-            <div>
-                <h4>Цена</h4>
-                <h4>0.4 ETH</h4>
-            </div>
+        <div>
+          <h4>Цена</h4>
+          <h4>{price} ETH</h4>
         </div>
-        <div className={css.rightItems}>
-            <div>
-                <h4>
-                    2.223 <span>ETH</span>
-                </h4>
-            </div>
-            <div>
-                <p>+57%</p>
-            </div>
+      </div>
+      <div className={css.rightItems}>
+        <div>
+          <h4>
+            {total} <span>ETH</span>
+          </h4>
         </div>
+        <div>
+          <p>+{percentage}%</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
