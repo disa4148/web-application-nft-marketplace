648e63be582e6a57844c8bdddd79c62cf03c2aae
disa4148 <denis.nikolaenko.2004@mail.ru>
2024-05-15 11:32:37 +0700

    fix(dropdowns): fix bug with menu closing on free area

--- src/app/[locale]/_components/header/_components/Dropdowns/BurgerDropdown.tsx
+++ src/app/[locale]/_components/header/_components/Dropdowns/BurgerDropdown.tsx
@@ -1,25 +1,46 @@
 'use client';
-import css from './Dropdowns.module.scss'
-import { useState } from 'react';
+import css from './Dropdowns.module.scss';
+
+import { useState, useEffect, useRef, MouseEvent as ReactMouseEvent } from 'react';
 import { useTranslations, useLocale } from 'next-intl';
 
 import LanguageSwitcher from './LanguageSwitcher';
-
 import Link from 'next/link';
 
 import { Globe, Headset, Menu, X, Heart } from 'lucide-react';
 
 export default function BurgerDropdown(): JSX.Element {
-  const locale = useLocale();
-  const [isOpen, setIsOpen] = useState(false);
   const t = useTranslations('header.dropdown.burgerMenu');
+  const locale = useLocale();
+  
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
 
-  const toggleMenu = () => {
-    setIsOpen((prevIsOpen) => !prevIsOpen);
+  const toggleMenu = (event: ReactMouseEvent) => {
+    event.stopPropagation();
+    setTimeout(() => {
+      setIsOpen((prevIsOpen) => !prevIsOpen);
+    }, 0);
   };
 
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener('click', handleClickOutside);
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, [isOpen]);
+
   return (
-    <div className={css.dropdown}>
+    <div className={css.dropdown} ref={dropdownRef}>
       <div className={css.trigger} onClick={toggleMenu}>
         {isOpen ? (
           <X width={26} height={26} className="invert" />
@@ -34,7 +55,9 @@ export default function BurgerDropdown(): JSX.Element {
               <div className={css.icon}>
                 <Heart width={20} height={20} className="invert" />
               </div>
-              <Link href={`/${locale}/favoritesNft`}>{t('favorites.title')}</Link>
+              <Link href={`/${locale}/favoritesNft`}>
+                {t('favorites.title')}
+              </Link>
             </div>
           </div>
           <div className={css.item}>
--- src/app/[locale]/_components/header/_components/Dropdowns/ProfileDropdown.tsx
+++ src/app/[locale]/_components/header/_components/Dropdowns/ProfileDropdown.tsx
@@ -1,6 +1,6 @@
 'use client';
 import css from './Dropdowns.module.scss';
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useTranslations, useLocale } from 'next-intl';
 
 import { cn } from '@/shared/lib/utils';
@@ -12,16 +12,34 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/shared/ui/avatar';
 import { Button } from '@/shared/ui/button';
 
 export default function ProfileDropdown(): JSX.Element {
-  const [isOpen, setIsOpen] = useState(false);
   const t = useTranslations('header.dropdown.profileMenu');
   const locale = useLocale();
+
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
 
   const toggleMenu = () => {
     setIsOpen((prevIsOpen) => !prevIsOpen);
   };
 
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener('click', handleClickOutside);
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
+
   return (
-    <div className={css.dropdown}>
+    <div className={css.dropdown} ref={dropdownRef}>
       <div onClick={toggleMenu}>
         <Avatar>
           <AvatarImage
