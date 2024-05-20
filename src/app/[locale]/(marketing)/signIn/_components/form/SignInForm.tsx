bfd84df4193902768b3168386fe3c568bcd4f079
disa4148 <denis.nikolaenko.2004@mail.ru>
2024-05-20 21:25:24 +0700

    feat(signIn): data completion logic & ui components

--- src/app/[locale]/(marketing)/signIn/_components/form/SignInForm.tsx
+++ src/app/[locale]/(marketing)/signIn/_components/form/SignInForm.tsx
@@ -6,31 +6,139 @@ import { useTranslations, useLocale } from 'next-intl';
 import { Input } from '@/shared/ui/input';
 import { Button } from '@/shared/ui/button';
 import { Checkbox } from '@/shared/ui/checkbox';
+import { Form, FormControl, FormField, FormItem } from '@/shared/ui/form';
+
 import Link from 'next/link';
+import { toast } from 'sonner';
+
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useEffect, useState } from 'react';
+
+type FieldErrors = {
+  [key: string]: any | undefined;
+};
 
 export default function SignInForm(): JSX.Element {
   const t = useTranslations('signIn');
   const locale = useLocale();
+
+  const formSchema = z.object({
+    login: z.string().min(1, {
+      message: t('messages.login'),
+    }),
+    password: z.string().min(1, {
+      message: t('messages.password'),
+    }),
+    remember: z.boolean().default(false),
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      login: '',
+      password: '',
+      remember: false,
+    },
+  });
+
+  const onSubmit = (data: z.infer<typeof formSchema>) => {
+    const payload = {
+      login: data.login,
+      password: data.password,
+      remember: data.remember,
+    };
+    console.log('Payload:', payload);
+    toast.success(t('messages.success'));
+  };
+
+  const [isErrorsShown, setIsErrorsShown] = useState<boolean>(false);
+  const errors: FieldErrors = form.formState.errors;
+
+  useEffect(() => {
+    if (!isErrorsShown) return;
+    for (const field in errors) {
+      const errorMessage = errors[field]?.message;
+      if (errorMessage) {
+        toast.error(errorMessage, { position: 'bottom-right' });
+      }
+    }
+    setIsErrorsShown(false);
+  }, [isErrorsShown]);
+
   return (
-    <div>
-      <Input type="text" placeholder={t('input.login')} />
-      <Input type="password" placeholder={t('input.password')} />
-      <div>
-        <Checkbox id="terms" />
-        <label
-          htmlFor="terms"
-          className="text-sm font-light leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className={css.form}>
+        <FormField
+          control={form.control}
+          name="login"
+          render={({ field }) => {
+            return (
+              <FormItem>
+                <FormControl>
+                  <Input placeholder={t('input.login')} {...field} />
+                </FormControl>
+              </FormItem>
+            );
+          }}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => {
+            return (
+              <FormControl>
+                <Input
+                  type="password"
+                  className={css.password}
+                  placeholder={t('input.password')}
+                  {...field}
+                />
+              </FormControl>
+            );
+          }}
+        />
+        <FormField
+          control={form.control}
+          name="remember"
+          render={({ field }) => {
+            return (
+              <FormItem>
+                <FormControl>
+                  <div className={css.checkbox}>
+                    <Checkbox
+                      id="terms"
+                      checked={field.value}
+                      onCheckedChange={field.onChange}
+                    />
+                    <label
+                      htmlFor="terms"
+                      className="text-sm font-light leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                    >
+                      {t('input.rememberMe')}
+                    </label>
+                  </div>
+                </FormControl>
+              </FormItem>
+            );
+          }}
+        />
+        <Button
+          onClick={() => setIsErrorsShown(true)}
+          type="submit"
+          className={css.styleButton}
+          variant={'default'}
         >
-          {t('input.rememberMe')}
-        </label>
-      </div>
-      <Button className={css.styleButton} variant={'default'}>
-        {t('buttonSignIn.text')}{' '}
-      </Button>
-      <div className={css.linkSignUp}>
-        <h1>{t('linkSignUp.text')}</h1>
-        <Link className={css.link} href={`/${locale}/signup`}>{t('linkSignUp.button')}</Link>
-      </div>
-    </div>
+          {t('buttonSignIn.text')}{' '}
+        </Button>
+        <div className={css.linkSignUp}>
+          <h1>{t('linkSignUp.text')}</h1>
+          <Link className={css.link} href={`/${locale}/signup`}>
+            {t('linkSignUp.button')}
+          </Link>
+        </div>
+      </form>
+    </Form>
   );
 }
