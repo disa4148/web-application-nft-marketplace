296e8aab2bb5235c15ac5357aff29f3879b8b5a4
disa4148 <denis.nikolaenko.2004@mail.ru>
2024-06-01 13:53:23 +0700

    feat(app): auth middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,35 @@
-import createMiddleware from 'next-intl/middleware';
- 
-export default createMiddleware({
-  // A list of all locales that are supported
-  locales: ['en', 'ru'],
- 
-  // Used when no locale matches
-  defaultLocale: 'en'
+import createIntlMiddleware from 'next-intl/middleware';
+import { NextRequest, NextResponse } from 'next/server';
+
+const locales = ['en', 'ru'];
+const publicPages = ['/', '/signin', '/signup'];
+
+const intlMiddleware = createIntlMiddleware({
+  locales,
+  defaultLocale: 'ru'
 });
- 
+
+export default async function middleware(req: NextRequest) {
+  const publicPathnameRegex = RegExp(
+    `^(/(${locales.join('|')}))?(${publicPages
+      .flatMap((p) => (p === '/' ? ['', '/'] : p))
+      .join('|')})/?$`,
+    'i'
+  );
+  const isPublicPage = publicPathnameRegex.test(req.nextUrl.pathname);
+
+  let isSignedIn = false;
+
+    const accessToken = req.cookies.get('accessToken');
+    isSignedIn = accessToken ? true : false;
+
+  if (isPublicPage || isSignedIn) {
+    return await intlMiddleware(req);
+  } else {
+    return NextResponse.redirect(new URL('/signin', req.url));
+  }
+}
+
 export const config = {
-  // Match only internationalized pathnames
-  matcher: ['/', '/(ru|en)/:path*']
+  matcher: ['/((?!api|_next|.*\\..*).*)']
 };
