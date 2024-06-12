2b368ffe82d30a084f84c416cb7908c3d71591ae
ssshpaklevka <apactop2004@gmail.com>
2024-06-12 15:47:14 +0700

    feat(selectBank): error

--- src/app/[locale]/_components/modalWalletConclusion/_components/selectBank.tsx
+++ src/app/[locale]/_components/modalWalletConclusion/_components/selectBank.tsx
@@ -7,19 +7,21 @@ import { useQueryConclusionMutation } from '@/shared/redux/payment/replenishment
 import { toast } from 'sonner';
 import { cn } from '@/shared/lib/utils';
 import { useTranslations } from 'next-intl';
+import { nullable } from 'zod';
 
 const initialBank = {
   value: 'card',
-  name: "Интернет-банк\nОплата",
-  nameExamUp: "Интернет-банк",
-  nameExamDw: "Оплата(Russia)"
+  name: 'Интернет-банк\nОплата',
+  nameExamUp: 'Интернет-банк',
+  nameExamDw: 'Оплата(Russia)',
 };
 
 export default function SelectBank() {
+  const t = useTranslations('header.dropdown.walletMenu.modalWithdraw');
 
-  const t = useTranslations("header.dropdown.walletMenu.modalWithdraw")
-
-  const [selectedBank, setSelectedBank] = useState<BankDetails | null>(initialBank);
+  const [selectedBank, setSelectedBank] = useState<BankDetails | null>(
+    initialBank,
+  );
   const [amount, setAmount] = useState('');
   const [cardNumber, setCardNumber] = useState('');
   const [isWithdrawalImpossible, setIsWithdrawalImpossible] = useState(false);
@@ -28,22 +30,24 @@ export default function SelectBank() {
 
   const handleAmountChange = (e: ChangeEvent<HTMLInputElement>) => {
     setAmount(e.target.value);
+
+    if (e.target.value === '0') {
+      toast.error(t("amountError"));
+    }
   };
 
   const handleCardChange = (e: ChangeEvent<HTMLInputElement>) => {
     setCardNumber(e.target.value);
   };
 
-
   const handleSubmit = async () => {
     const amountNumber = parseFloat(amount);
 
     if (isNaN(amountNumber)) {
-      toast.error('Сумма должна быть числом');
       return;
     }
 
-    console.log(selectedBank)
+    console.log(selectedBank);
 
     const data = {
       amount: amountNumber,
@@ -53,48 +57,50 @@ export default function SelectBank() {
 
     try {
       await queryConclusion(data).unwrap();
-      toast.success('Успешно отправлено');
+      setAmount('');
+      setCardNumber('');
       console.log(data);
       setIsWithdrawalImpossible(true);
     } catch (e: any) {
       if (e.data && e.data.message) {
-        toast.error(e.data.message);
         setIsWithdrawalImpossible(true);
+        setAmount('');
+        setCardNumber('');
       }
     }
   };
 
   return (
     <div className={css.page}>
-    <h1 className='text-1-text-white-100'>{t("withdraw")}</h1>
-    <div className={css.wrapper}>
-      <Bank selectedBank={selectedBank} setSelectedBank={setSelectedBank} />
-      <div className={css.sending}>
-        <Input
-          className={css.input}
-          value={amount}
-          onChange={handleAmountChange}
-          placeholder={t("enterAmount")}
-          type="number"
-        />
-        <Input
-        onChange={handleCardChange}
-          className={css.input}
-          value={cardNumber}
-          placeholder={t("accountNumber")}
-        />
-        <Button
-          className={cn(css.button, 'bg-1-gradient')}
-          onClick={handleSubmit}
-          disabled={isLoading}
-        >
-          {isLoading ? t("loadingButton") : t("withdrawButton")}
-        </Button>
-        {isWithdrawalImpossible && (
-            <p className={css.errorMessage}>{t("failedWith")}</p>
+      <h1 className="text-1-text-white-100">{t('withdraw')}</h1>
+      <div className={css.wrapper}>
+        <Bank selectedBank={selectedBank} setSelectedBank={setSelectedBank} />
+        <div className={css.sending}>
+          <Input
+            className={css.input}
+            value={amount}
+            onChange={handleAmountChange}
+            placeholder={t('enterAmount')}
+            type="number"
+          />
+          <Input
+            onChange={handleCardChange}
+            className={css.input}
+            value={cardNumber}
+            placeholder={t('accountNumber')}
+          />
+          <Button
+            className={cn(css.button, 'bg-1-gradient')}
+            onClick={handleSubmit}
+            disabled={isLoading}
+          >
+            {isLoading ? t('loadingButton') : t('withdrawButton')}
+          </Button>
+          {isWithdrawalImpossible && (
+            <p className={css.errorMessage}>{t('failedWith')}</p>
           )}
+        </div>
       </div>
-    </div>
     </div>
   );
 }
