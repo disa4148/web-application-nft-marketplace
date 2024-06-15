7d6cd1df9299691f9b994e649173239d646ecfda
Kovalenko Rodion <167234046+clownlessmode@users.noreply.github.com>
2024-06-15 20:54:31 +0300

    fix

--- src/app/[locale]/_components/header/HeaderLayout.tsx
+++ src/app/[locale]/_components/header/HeaderLayout.tsx
@@ -3,14 +3,8 @@ import { RootState } from '@/shared/redux/store';
 import Authorized from './Authorized';
 import Unauthorized from './Unauthorized';
 import { useDispatch, useSelector } from 'react-redux';
-import { useGetMeQuery } from '@/shared/redux/features/authApi';
-import { setUser } from '@/shared/redux/slices/authSlice';
-import { getUser } from '../../(marketing)/messenger/axios/axios';
-import { useEffect } from 'react';
 
 const HeaderLayout = () => {
-  const dispatch = useDispatch();
-
   const isSignedIn = useSelector((state: RootState) => state.auth.isSignedIn);
   const user = useSelector((state: RootState) => state.auth.user);
 
